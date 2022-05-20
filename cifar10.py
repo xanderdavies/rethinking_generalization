f70e81449832bdb2ddc6a27adc19b77581facd09
Davies <ald339@login03.o2.rc.hms.harvard.edu>
2022-05-19 21:54:39 -0400

    add corrupted_labels mode

--- cifar10.py
+++ cifar10.py
@@ -2,8 +2,9 @@
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
+import numpy as np
 
-def get_dataloaders(batch_size, corruption=None):
+def get_dataloaders(batch_size, corrupted_labels=False):
 
     trans = transforms.Compose(
         [
@@ -16,6 +17,11 @@ def get_dataloaders(batch_size, corruption=None):
 
     train_set = CIFAR10("./data", train=True, download=True, transform=trans)
     test_set = CIFAR10("./data", train=False, download=True, transform=trans)
+
+    if corrupted_labels:
+        np.random.seed(0)
+        train_set.targets = np.random.choice(10, len(train_set.targets))
+        test_set.targets = np.random.choice(10, len(test_set.targets))
 
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
